3226d700d034f48969fbc3ec4eca845ff287729e
MelvinCou <melvin.courjaud@epitech.eu>
2023-07-03 15:42:41 +0200

    Add microphone component

---
+++ front/views/TestMicView.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import PlayMic from '@/components/sound/PlayMic.vue';
+</script>
+
+<template>
+    <div>
+        <PlayMic :enable-canvas="true" />
+    </div>
+</template>

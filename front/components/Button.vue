1f18a698846b2c54db766e28aa57549d8363b75c
Jibe LELEVIER <devme@MSI.(none)>
2023-07-03 11:45:28 +0200

    Add music xml collection

--- front/components/Button.vue
+++ front/components/Button.vue
@@ -7,33 +7,32 @@
 </template>
 
 <script>
-    export default {
-        name: 'Button',
-        methods: {
-            handleClick() {
-                console.log("hello world")
+export default {
+    name: 'Button',
+    methods: {
+        handleClick() {
+            console.log('hello world');
         },
     },
 };
 </script>
 
-
 <style lang="css" scoped>
 .button {
-  color: #4C324D;
-  background-color: #00FF75;
-  padding: 12px 24px;
-  font-size: 25px;
-  border-radius: 4px;
-  font-weight: 700;
-  border: 4px solid #4C324D;
-  box-shadow: 8px 8px #4C324D;
-  border-radius: 4px;
-  transition: background-color 0.3s ease;
+    color: #4c324d;
+    background-color: #00ff75;
+    padding: 12px 24px;
+    font-size: 25px;
+    border-radius: 4px;
+    font-weight: 700;
+    border: 4px solid #4c324d;
+    box-shadow: 8px 8px #4c324d;
+    border-radius: 4px;
+    transition: background-color 0.3s ease;
 }
 
 .button:hover {
-  background-color: #FAFF00;
-  color: #000;
+    background-color: #faff00;
+    color: #000;
 }
 </style>

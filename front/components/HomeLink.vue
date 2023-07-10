f14d072dd1709fddbe18c62f3565182122fbf1bd
Yanis Guaye <yanis.guaye@epitech.eu>
2023-07-10 16:56:50 +0200

    Make the page fit within a mobile screen
    Linked #44

--- front/components/HomeLink.vue
+++ front/components/HomeLink.vue
@@ -24,10 +24,12 @@ const emit = defineEmits<{
             class="absolute left-[-100px]"
         ></SquareIcon>
         <RouterLink :to="props.to" class="hover:bg-link-selection focus:bg-link-selection outline-none">
-            <h2 class="font-extrabold text-7xl bg-inherit rounded-full transition-colors duration-200 ease-in">
+            <h2
+                class="font-extrabold text-5xl md:text-7xl bg-inherit rounded-full transition-colors duration-200 ease-in"
+            >
                 <slot name="default" />
             </h2>
-            <h3 class="text-3xl">
+            <h3 class="text-2xl md:text-3xl">
                 <slot name="subtitle" />
             </h3>
         </RouterLink>

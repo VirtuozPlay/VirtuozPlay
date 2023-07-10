747914aca39b96f74435b75ea1efcf4f1c93d0c2
Yanis Guaye <yanis.guaye@epitech.eu>
2023-07-11 01:29:06 +0200

    Add the <HeaderDrawer> component
    Linked #44

---
+++ front/components/HeaderLink.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+export interface Props {
+    name: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <RouterLink :to="{ name }" :class="{ underline: $route.name === name }" class="text-lg md:text-xl font-bold">
+        <slot></slot>
+    </RouterLink>
+</template>

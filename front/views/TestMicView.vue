69671ff3f5ce72af18b510934841ae830565ffa4
MelvinCou <melvin.courjaud@epitech.eu>
2023-07-10 20:35:56 +0200

    Check if note has already been detected

--- front/views/TestMicView.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import PlayMic from '@/components/sound/PlayMic.vue';
-</script>
-
-<template>
-    <div>
-        <PlayMic :enable-canvas="true" />
-    </div>
-</template>

8e95178e24574359540026246e3177b7f5c6e7ee
MelvinCou <melvin.courjaud@epitech.eu>
2023-08-29 14:54:01 +0200

    Add populating song migration

---
+++ migrations/20230829122814_populate_song.up.sql
@@ -0,0 +1,6 @@
+INSERT INTO song
+(id, nano_id, title, created_at, updated_at, img_url, music_path, url)
+VALUES 
+    (1001, 'h8rHA-Q0dD5dBbY1L2Fzf', 'Can Can', CURRENT_DATE, CURRENT_DATE, '../../images/cancan.jpg', '/assets/music/cancan/cancan.mp3', 'cancan'),
+    (1002, 'QyPqpmFqWC4uGInmkodgP', 'Corinna Corinna', CURRENT_DATE, CURRENT_DATE, '../../images/corinna.jpg', '/assets/music/cancan/corinna.mp3', 'corinna'),
+    (1003, 'SciuYTreZAmlKJhg4Tk0x', 'Sweet Home Alabama', CURRENT_DATE, CURRENT_DATE, '../../images/alabama.jpg', '/assets/music/cancan/alabama.mp3', 'alabama');
